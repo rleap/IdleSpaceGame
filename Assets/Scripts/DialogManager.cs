5e7e1eafa27c8166b343079a785e13d66b6a63b1
Robert Leap <robertleap@gmail.com>
2018-12-21 17:04:14 -0800

    Added stub scenes

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogText;
+
+    private Queue<string> sentences;
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+        sentences = new Queue<string>();
+
+	}
+
+    public void StartDialog(Dialog dialog)
+    {
+        animator.SetBool("IsOpen", true);
+
+        nameText.text = dialog.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;  // wait single frame
+        }
+    }
+
+    void EndDialog()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
